a9547251fb33ee13470a715b6a56277330c40a4a
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-07-27 03:38:42 +0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { PageNotFoundComponent } from './auth/page-not-found/page-not-found.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { AllProblemsComponent } from './problemset/all-problems/all-problems.component';
+
+const routes: Routes = [
+  {
+    path: 'accounts/login',
+    component: SigninComponent
+  },
+  {
+    path: 'accounts/register',
+    component: SignupComponent
+  },
+  {
+    path: 'accounts/forgot-password',
+    component: ForgotPasswordComponent
+  },
+  {
+    path: 'problemset/all',
+    component: AllProblemsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'accounts/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [],
